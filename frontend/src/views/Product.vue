d0c20d36f65f1767e8fbede2984ad501a1ab8a47
solaiman-hridoy <libnum10@gmail.com>
2021-04-11 19:21:46 +0600

    ADD a loading bar

--- frontend/src/views/Product.vue
+++ frontend/src/views/Product.vue
@@ -44,11 +44,13 @@ export default {
         this.getProduct()
     },
     methods: {
-        getProduct(){
+        async getProduct(){
+            this.$store.commit('setIsLoading', true)
+
             const category_slug = this.$route.params.category_slug
             const product_slug = this.$route.params.product_slug
 
-            axios
+            await axios
                 .get(`/api/v1/products/${category_slug}/${product_slug}`)
                 .then(response => {
                     this.product = response.data
@@ -56,6 +58,9 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
+
+            this.$store.commit('setIsLoading', false)
+
         },
         getToCart() {
             if (isNaN(this.quantity) || this.quantity < 1) {
