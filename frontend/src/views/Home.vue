b603c7560a2ae7bde05e07fceccbdcbe69a1a8a3
solaiman-hridoy <libnum10@gmail.com>
2021-04-11 19:25:46 +0600

    ADD a loading bar to home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -53,8 +53,9 @@ export default {
     this.getLatestProducts()
   },
   methods: {
-    getLatestProducts() {
-      axios
+    async getLatestProducts() {
+    this.$store.commit('setIsLoading', true)
+    await axios
       .get('/api/v1/latest-products')
       .then(response => {
         this.latestProducts = response.data
@@ -62,6 +63,9 @@ export default {
       .catch(error => {
         console.log(error)
       })
+
+      this.$store.commit('setIsLoading', false)
+
     }
   }
 }
