ceb921948d394cd71072fa96c01f9ec8db4636bb
solaiman-hridoy <libnum10@gmail.com>
2021-04-11 17:31:18 +0600

    ADD functionality to cuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -10,6 +10,23 @@ export default createStore({
     isLoading: false
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem('cart')) {
+        state.cart = JSON.parse(localStorage.getItem('cart'))
+      } else {
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+      }
+    },
+    addToCart(state, item) {
+      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+
+      if (exists.length) {
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      } else {
+        state.cart.item.push(item)
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
   },
   actions: {
   },
